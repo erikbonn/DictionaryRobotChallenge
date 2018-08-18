a681af8d6440d980ac7e990a87026ea1199bcca9
erikbonn <ebonn89@gmail.com>
2018-08-18 00:12:47 -0600

    Manage dependencies for react-router, webpack, and react

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require(;rect-router);
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
 
 var objOne = {
-  name: 'Erik',
+  name: 'Erik'
 };
 
 var objTwo = {
@@ -19,6 +19,6 @@ ReactDOM.render(
     <Router history={hashHistory}>
       <Route path="/" component={Main}>
       </Route>
-    </Router>
+    </Router>,
     document.getElementById('app')
 );
